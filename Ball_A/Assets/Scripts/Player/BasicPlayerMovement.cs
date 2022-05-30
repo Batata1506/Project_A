ae9be353153d7fcd7c05284f695f47f216f4e25f
Batata1506 <tahalsalihi@gmail.com>
2022-05-30 01:45:07 +0100

    Taha's physics V.1.0

--- Ball_A/Assets/Scripts/Player/BasicPlayerMovement.cs
+++ Ball_A/Assets/Scripts/Player/BasicPlayerMovement.cs
@@ -1,16 +1,21 @@
+using JetBrains.Rider.Unity.Editor;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BasicPlayerMovement : MonoBehaviour
 {
     private CircleCollider2D circleCollider;
     private Rigidbody2D body;
-    private float horizontalMovement;
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float jumpHeight;
+    private Vector2 _move;
+    [SerializeField] private float movementSpeed = 200f;
+    [SerializeField] private float jumpHeight = 100f;
     [SerializeField] private LayerMask groundLayer;
+    private float _maxSpeed =  220f;
     private Animator anim;
+    private float horiDirection;
+    private bool  direction ;
 
     // Awake is called when the script instance is being loaded
     private void Awake()
@@ -30,21 +35,28 @@ public class BasicPlayerMovement : MonoBehaviour
     // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
     private void FixedUpdate()
     {
-        if (horizontalMovement > 0)
-        {
-            MoveRight();
-        }
-        else if (horizontalMovement < 0)
-        {
-            MoveLeft();
-        }
-
+        Move();
         if(Input.GetKey(KeyCode.Space) && IsGrounded())
         {
             Jump();
             anim.SetTrigger("jump");
+
+             if (Input.GetKey(KeyCode.F) )
+        {
+            body.gravityScale = 5f;
+         
+            if (IsGrounded())
+            {
+                body.gravityScale = 1f;
+                Jump();
+                anim.SetTrigger("jump");
+            }
+          
         }
-        if(body.velocity.y  != 0 && IsGrounded() == false)
+           
+        }
+
+        if ( IsGrounded() == false)
         {
             body.rotation = 0;
             body.freezeRotation = true;
@@ -53,31 +65,36 @@ public class BasicPlayerMovement : MonoBehaviour
         {
             body.freezeRotation = false;
         }
+
+      
     }
 
     // Update is called every frame, if the MonoBehaviour is enabled
     private void Update()
     {
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
-        if(body.velocity.x != 0)
+        _move = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        horiDirection = Input.GetAxis("Horizontal");
+        direction = (body.velocity.x > 0f && horiDirection < 0f) || (body.velocity.x < 0f && horiDirection > 0f);
+
+        if (Mathf.Abs(horiDirection) < 0.4f || direction)
         {
             anim.SetBool("run", true);
         }
-        else if(body.velocity.x == 0)
-        {
-            anim.SetBool("run", false);
-        }
+        else anim.SetBool("run", false);
+
+
     }
 
-    private void MoveRight()
+    private void Move()
     {
-        body.AddForce(new Vector2(movementSpeed, body.velocity.y));
+         body.AddForce(_move*movementSpeed*Time.deltaTime, ForceMode2D.Force);
+        if(Mathf.Abs(body.velocity.x) > _maxSpeed)
+            body.velocity = new Vector2(Mathf.Sign(body.velocity.x* _maxSpeed), body.velocity.y  );
+        
+
+       // body.velocity = new Vector2(_move * movementSpeed * Time.deltaTime, body.velocity.y);
     }
-    private void MoveLeft()
-    {
-        body.AddForce(new Vector2(-movementSpeed, body.velocity.y));
-    }
-    
+   
     private void Jump()
     {
         body.AddForce(new Vector2(body.velocity.x, jumpHeight));
