27ec2e3127c841fa218188c06a458a363ea04bc0
Ominousss <ominiousblast@gmail.com>
2022-06-18 06:05:58 +0100

    Level updated

--- Ball_A/Assets/Scripts/Camera/ParallaxBackground.cs
+++ Ball_A/Assets/Scripts/Camera/ParallaxBackground.cs
@@ -13,6 +13,9 @@ public class ParallaxBackground : MonoBehaviour
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
         
     }
 
@@ -23,5 +26,11 @@ public class ParallaxBackground : MonoBehaviour
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
         transform.position += new Vector3(deltaMovement.x * parallexEffectMultiplier.x, deltaMovement.y * parallexEffectMultiplier.y);
         lastCameraPosition = cameraTransform.position;
+
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x + 20) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x, transform.position.y);
+        }
     }
 }
